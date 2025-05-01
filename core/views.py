d54c52a587680cdca966f2b743a1c9f7d313c6c2
Eren Celik <erencelik2426@gmail.com>
2025-05-01 13:04:02 +0300

    feat : customize views.py

--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def index(request):
-    return render(request, 'templates\\index.html')
+    return render(request, 'templates/index.html')
 
 def contact(request):
-    return render(request, 'templates\\contact.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        print(f"İletişim Formu: {name} - {email} - {subject} - {message}")
+        messages.success(request, 'Your message has been sent.')
+        return redirect('/#contact')
+    return redirect('/#contact')

c0943c8942edb01e50930e0fd33f3fc338f6cdfb
Eren Celik <erencelik2426@gmail.com>
2025-05-02 23:50:49 +0300

    refactor : application of genereal settings

--- core/views.py
+++ core/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from core.models import GeneralSetting
 
 def index(request):
-    return render(request, 'templates/index.html')
+
+    site_title = GeneralSetting.objects.get(name='site_title').parameter
+    site_keywords = GeneralSetting.objects.get(name='site_keywords').parameter
+    site_description = GeneralSetting.objects.get(name='site_description').parameter
+    home_banner_name = GeneralSetting.objects.get(name='home_banner_name').parameter
+    home_banner_description = GeneralSetting.objects.get(name='home_banner_description').parameter
+    about_me_capabilities = GeneralSetting.objects.get(name='about_me_capabilities').parameter
+
+
+    context = {
+        'site_title': site_title,
+        'site_keywords':site_keywords,
+        'site_description': site_description,
+        'home_banner_name': home_banner_name,
+        'home_banner_description': home_banner_description,
+        'about_me_capabilities': about_me_capabilities,
+    }
+    return render(request, 'templates/index.html' , context=context)
 
 def contact(request):
     if request.method == 'POST':

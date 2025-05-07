894f47c4e5f6378a208ddbe4f6013d82611744bf
Eren Celik <erencelik2426@gmail.com>
2025-05-07 20:16:17 +0300

    feat : create image setting model

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from core.models import GeneralSetting
+from core.models import GeneralSetting, ImageSetting
+
 
 def index(request):
 
@@ -11,6 +12,9 @@ def index(request):
     home_banner_description = GeneralSetting.objects.get(name='home_banner_description').parameter
     about_me_capabilities = GeneralSetting.objects.get(name='about_me_capabilities').parameter
 
+    home_favicon= ImageSetting.objects.get(name='home_favicon').file
+    personal_picture = ImageSetting.objects.get(name='personal_picture').file
+
 
     context = {
         'site_title': site_title,
@@ -19,6 +23,8 @@ def index(request):
         'home_banner_name': home_banner_name,
         'home_banner_description': home_banner_description,
         'about_me_capabilities': about_me_capabilities,
+        'home_favicon': home_favicon,
+        'personal_picture': personal_picture,
     }
     return render(request, 'templates/index.html' , context=context)
 

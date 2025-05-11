5a90ab84a74a7708222f738df787d2a734e21399
Eren Celik <erencelik2426@gmail.com>
2025-05-12 01:24:20 +0300

    feat  : create message model

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
+from django.contrib.messages.api import success
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from core.models import GeneralSetting, ImageSetting, Skill , Capabilities
+from core.models import GeneralSetting, ImageSetting, Skill, Capabilities, Message
 
 
 def index(request):
-
     site_title = GeneralSetting.objects.get(name='site_title').parameter
     site_keywords = GeneralSetting.objects.get(name='site_keywords').parameter
     site_description = GeneralSetting.objects.get(name='site_description').parameter
@@ -12,7 +13,7 @@ def index(request):
     home_banner_description = GeneralSetting.objects.get(name='home_banner_description').parameter
     about_me_capabilities = GeneralSetting.objects.get(name='about_me_capabilities').parameter
 
-    home_favicon= ImageSetting.objects.get(name='home_favicon').file
+    home_favicon = ImageSetting.objects.get(name='home_favicon').file
     personal_picture = ImageSetting.objects.get(name='personal_picture').file
 
     skills = Skill.objects.all().order_by('order')
@@ -20,7 +21,7 @@ def index(request):
 
     context = {
         'site_title': site_title,
-        'site_keywords':site_keywords,
+        'site_keywords': site_keywords,
         'site_description': site_description,
         'home_banner_name': home_banner_name,
         'home_banner_description': home_banner_description,
@@ -30,16 +31,36 @@ def index(request):
         'skills': skills,
         'capabilities': capabilities,
     }
-    return render(request, 'templates/index.html' , context=context)
+    return render(request, 'templates/index.html', context=context)
+
+
+def contact_form(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        message_text = request.POST.get("message")
+
+        Message.objects.create(
+            name=name,
+            email=email,
+            phone=phone,
+            message=message_text
+        )
+
+        messages.success(request, 'Message has been sent.')
+        return render(request , 'templates/index.html')
+    else:
+        success = False
+        message = 'Request method is not valid.'
+
+    context = {
+        'success': success,
+        'message': message,
+    }
+
+    return JsonResponse(context)
+
 
 def contact(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
-
-        print(f"Communication Form: {name} - {email} - {subject} - {message}")
-        messages.success(request, 'Your message has been sent!')
-        return redirect('/#contact')
-    return redirect('/#contact')
+    return render(request, 'templates/index.html')
